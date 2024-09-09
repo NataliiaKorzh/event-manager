03c0120e03a8fafcd8e1eed12b8b84c27abeecea
Nataliia <clarioruberia@gmail.com>
2024-09-09 09:20:05 +0300

    add events & user apps to settings.py

--- event_manager/settings.py
+++ event_manager/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "events",
+    "user"
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,8 @@ ROOT_URLCONF = "event_manager.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates']
+        ,
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
