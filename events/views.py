4b8a9d34e70ccb398866f8df8edf2e8d6699a2c3
Nataliia <clarioruberia@gmail.com>
2024-09-09 12:41:10 +0300

    add filters and searching

--- events/views.py
+++ events/views.py
@@ -1,5 +1,7 @@
-from rest_framework import viewsets, permissions, generics, mixins
+from rest_framework import viewsets, permissions, mixins, filters
 from rest_framework.viewsets import GenericViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiTypes
 
 from .models import Event, EventRegistration
 from .serializers import EventSerializer, EventRegistrationSerializer
@@ -9,6 +11,32 @@ class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+    filterset_fields = ["date", "location"]
+    search_fields = ["title", "description", "location"]
+    ordering_fields = ["date", "title"]
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="date",
+                type=OpenApiTypes.DATE,
+                description="Filter by date of the event (ex. ?date=2022-10-23)"
+            ),
+            OpenApiParameter(
+                name="location",
+                type=OpenApiTypes.STR,
+                description="Filter by event location (ex. ?location=New York)"
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
 
 
 class EventRegistrationViewSet(
