549e7ce28fd708ff244859565355cca5c6574986
Nataliia <clarioruberia@gmail.com>
2024-09-09 09:18:59 +0300

    create events app

---
+++ events/views.py
@@ -0,0 +1,16 @@
+from rest_framework import generics
+from .models import Event
+from .serializers import EventSerializer
+from rest_framework.permissions import IsAuthenticated
+
+
+class EventListCreateView(generics.ListCreateAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class EventRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    permission_classes = [IsAuthenticated]
